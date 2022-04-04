af430c14b2d60a35cd6970d56f94cc0b3a860b05
Kajune <ryuzoji21@gmail.com>
2022-04-04 15:47:41 +0900

    save & load functions

--- kodoku/trainer.py
+++ kodoku/trainer.py
@@ -1,11 +1,13 @@
 from typing import *
 from gym import Env, spaces
 import numpy as np
+import pickle
 
 import ray
+from ray.tune.logger import pretty_print
 import ray.rllib.agents as agents
 from ray.rllib.agents.trainer import Trainer
-from ray.tune.logger import pretty_print
+from ray.rllib.policy.policy import Policy
 from ray.rllib.env.env_context import EnvContext
 from ray.rllib.models import ModelCatalog, MODEL_DEFAULTS
 from ray.rllib.utils.typing import ResultDict
@@ -158,4 +160,81 @@ class KODOKUTrainer:
 			List: reward
 		"""
 		return self.trainer.callbacks.reward()
+
+
+	def get_policy(self, policy_id : str) -> Policy:
+		""" Get policy instance by id
 		
+		Args:
+		    policy_id (str): Policy id
+		"""
+		policy = self.trainer.get_policy(policy_id)
+		print(policy)
+		return policy
+	
+
+	def save_policy(self, path : str, policy_id : Optional[str] = None) -> None:
+		""" Save indivisual or whole policy into file
+		
+		Args:
+		    path (str): Save file path
+		    policy_id (Optional[str], optional): Policy id, if you want to select specific policy.
+		"""
+		with open(path, 'wb') as f:
+			if policy_id is not None:
+				pickle.dump({policy_id: self.get_policy(policy_id).get_weights()}, f)
+			else:
+				pickle.dump(
+					{ policy_id: self.get_policy(policy_id).get_weights() 
+						for policy_id in self.train_config["multiagent"]["policies"].keys()}, f)
+
+
+	def load_policy(self, path : str, policy_id : Optional[str] = None) -> None:
+		""" Load indivisual or whole policy from file
+		
+		Args:
+		    path (str): Load file path
+		    policy_id (Optional[str], optional): Policy id, if you want to select specific policy
+		"""
+		with open(path, 'rb') as f:
+			weights = pickle.load(f)
+			if policy_id is not None:
+				if policy_id in weights:
+					self.get_policy(policy_id).set_weights(weights[policy_id])
+				else:
+					print('Policy %s not found in %s. If you want to explicitly load policy with different name, use load_alternative_policy instead.' % (policy_id, path))
+			else:
+				for policy_id, weight in weights.items():
+					self.get_policy(policy_id).set_weights(weight)
+
+
+	def load_alternative_policy(self, path : str, from_policy_id : str, to_policy_id : str) -> None:
+		""" Explicitly load policy with different id
+		Note that policy's model must have exactly same parameters.
+		
+		Args:
+		    path (str): Load file path
+		    from_policy_id (str): Previous policy id
+		    to_policy_id (str): New policy id
+		"""
+		with open(path, 'rb') as f:
+			weights = pickle.load(f)
+			self.get_policy(to_policy_id).set_weights(weights[from_policy_id])
+
+
+	def save_checkpoint(self, path : str) -> None:
+		""" Save training checkpoint
+		
+		Args:
+		    path (str): Save dir path
+		"""
+		pickle.dump(self.trainer.__getstate__(), open(path, "wb"))
+
+
+	def load_checkpoint(self, path : str) -> None:
+		""" Load training checkpoint
+		
+		Args:
+		    path (str): Load dir path
+		"""
+		self.trainer.__setstate__(pickle.load(open(path, "rb")))
